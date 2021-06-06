b245bb600e2f1ea3dbdcbed171e9b1e61359b875
Eugene <56676493+Sentinelli@users.noreply.github.com>
2021-06-06 18:16:49 +0300

    init: init Q&A app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Question from './components/question/question';
+
+ReactDOM.render(
+  <>
+    <Question/>
+  </>,
+  document.getElementById('root')
+);
