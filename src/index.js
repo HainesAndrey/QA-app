86a66ec954fdbb9a2d5a97f7c46038a960a06481
Andrey <andrey.kappix.haines@gmail.com>
2021-06-13 21:20:13 +0300

    настройка роутинга по блокам вопросов part 2

--- src/index.js
+++ src/index.js
@@ -9,17 +9,47 @@ import './index.css';
 
 import Question from './components/question/question';
 
-import adultQuestions from './data/block3/adultQuestions.json';
-import childQuestions from './data/block3/childQuestions.json';
+import adultQuestions1 from './data/block1/adultQuestions.json';
+import childQuestions1 from './data/block1/childQuestions.json';
+
+import adultQuestions2 from './data/block2/adultQuestions.json';
+import childQuestions2 from './data/block2/childQuestions.json';
+
+import adultQuestions3 from './data/block3/adultQuestions.json';
+import childQuestions3 from './data/block3/childQuestions.json';
+
+import adultQuestions4 from './data/block4/adultQuestions.json';
+import childQuestions4 from './data/block4/childQuestions.json';
 
 ReactDOM.render(
   <Router>
       <Switch>
+          <Route path="/block1/adults">
+              <Question questions={adultQuestions1}/>
+          </Route>
+          <Route path="/block1/children">
+              <Question questions={childQuestions1}/>
+          </Route>
+
+          <Route path="/block2/adults">
+              <Question questions={adultQuestions2}/>
+          </Route>
+          <Route path="/block2/children">
+              <Question questions={childQuestions2}/>
+          </Route>
+
           <Route path="/block3/adults">
-              <Question questions={adultQuestions}/>
+              <Question questions={adultQuestions3}/>
           </Route>
           <Route path="/block3/children">
-              <Question questions={childQuestions}/>
+              <Question questions={childQuestions3}/>
+          </Route>
+
+          <Route path="/block4/adults">
+              <Question questions={adultQuestions4}/>
+          </Route>
+          <Route path="/block4/children">
+              <Question questions={childQuestions4}/>
           </Route>
       </Switch>
   </Router>,
